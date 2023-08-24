0d645dd2d6dc5bb0b7912a284620a609ddc98d62
Rylie Castell <rylie.castell@gmail.com>
2023-08-24 14:57:41 +0200

    add tests

--- src/test/java/JavaFibonacciTest.java
+++ src/test/java/JavaFibonacciTest.java
@@ -4,6 +4,33 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class JavaFibonacciTest {
     @Test
     void makeFibonacci_whenNumberIs0_return0(){
+      int n = 0;
+      int actual = JavaFibonacci.makeFibonacci(n);
+      int expected = 0;
+      assertEquals(expected, actual);
+    }
 
+    @Test
+    void makeFibonacci_whenNumberIs1_return1(){
+        int n = 1;
+        int actual = JavaFibonacci.makeFibonacci(n);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void makeFibonacci_whenNumberIs2_return1(){
+        int n = 2;
+        int actual = JavaFibonacci.makeFibonacci(n);
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void makeFibonacci_whenNumberIs8_return21(){
+        int n = 8;
+        int actual = JavaFibonacci.makeFibonacci(n);
+        int expected = 21;
+        assertEquals(expected, actual);
     }
 }
