2759830ca1a7ad0e9798253aa9710fca1179d734
Rylie Castell <rylie.castell@gmail.com>
2023-08-24 15:07:57 +0200

    add fibonacci method

--- src/main/java/JavaFibonacci.java
+++ src/main/java/JavaFibonacci.java
@@ -3,5 +3,23 @@ public class JavaFibonacci {
 
     }
 
+    public static int makeFibonacci(int n) {
+        int toBeAdded[] = {0, 1};
+        int fibonacci = 0;
 
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1) {
+            return 1;
+        }
+
+        for(int i = 1; i < n; i++){
+            fibonacci = toBeAdded[0] + toBeAdded[1];
+            toBeAdded[0] = toBeAdded[1];
+            toBeAdded[1] = fibonacci;
+        }
+        return fibonacci;
+
+    }
 }
