df6dc3f8f0dc02cf4f2d428679d0a607f80cc358
Rylie Castell <rylie.castell@gmail.com>
2023-08-24 15:23:38 +0200

    modify method for negative numbers

--- src/main/java/JavaFibonacci.java
+++ src/main/java/JavaFibonacci.java
@@ -4,9 +4,20 @@ public class JavaFibonacci {
     }
 
     public static int makeFibonacci(int n) {
-        int toBeAdded[] = {0, 1};
+        int[] toBeCalculated = {0, 1};
         int fibonacci = 0;
 
+        if(n < -1){
+          for(int i = -1; i > n; i--) {
+              fibonacci = toBeCalculated[0] - toBeCalculated[1];
+              toBeCalculated[0] = toBeCalculated[1];
+              toBeCalculated[1] = fibonacci;
+        }
+          return fibonacci;
+        }
+        if(n == -1){
+            return 1;
+        }
         if(n == 0){
             return 0;
         }
@@ -14,10 +25,12 @@ public class JavaFibonacci {
             return 1;
         }
 
+
+
         for(int i = 1; i < n; i++){
-            fibonacci = toBeAdded[0] + toBeAdded[1];
-            toBeAdded[0] = toBeAdded[1];
-            toBeAdded[1] = fibonacci;
+            fibonacci = toBeCalculated[0] + toBeCalculated[1];
+            toBeCalculated[0] = toBeCalculated[1];
+            toBeCalculated[1] = fibonacci;
         }
         return fibonacci;
 
